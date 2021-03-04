5849503a31b4d4aaf2d7cd14f13087a0ade38b23
sabrina071088 <73609836+sabrina071088@users.noreply.github.com>
2021-03-04 23:03:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var background1,backgroundimage
+
+
+function preload(){
+  
+}
+
+
+function setup() {
+  createCanvas(1200,1200);
+  background1=createSprite(600, 600, 1200, 1200);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
